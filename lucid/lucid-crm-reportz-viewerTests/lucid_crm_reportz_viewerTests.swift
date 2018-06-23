bf61dc98d30d7815d06b6a60d112467956207532
Darshan Gulur Srinivasa <darshan.srinivasa@accesscfa.com>
2018-06-23 13:18:19 -0400

    Setup

---
+++ lucid/lucid-crm-reportz-viewerTests/lucid_crm_reportz_viewerTests.swift
@@ -0,0 +1,36 @@
+//
+//  lucid_crm_reportz_viewerTests.swift
+//  lucid-crm-reportz-viewerTests
+//
+//  Created by Darshan Gulur Srinivasa on 6/23/18.
+//  Copyright © 2018 Lucid Infosystems LLC. All rights reserved.
+//
+
+import XCTest
+@testable import lucid_crm_reportz_viewer
+
+class lucid_crm_reportz_viewerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
